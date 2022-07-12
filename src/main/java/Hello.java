823ab8eb3491217f6b01c4175f95fed8044d15f2
camilo1111 <cdiaz29@uan.edu.co>
2022-07-12 17:56:11 -0500

    commit de prueba

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,7 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
-        System.out.println("Segundo mensaje");
-        System.out.println("Mensaje para push");
+
     }
 }
